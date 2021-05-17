331eb1d951562498f4b58b233c9c8d417ed4555b
uzzz1 <1075277345@qq.com>
2021-05-17 19:44:08 +0800

    提供了两种解决方案

--- 按位求和/sumAllUnits.py
+++ 按位求和/sumAllUnits.py
@@ -1,7 +1,9 @@
+#方法一
 class sumAll :
     def __init__(self,intN):
         self.value = intN
         self.sum = self.sumAllUnit(intN)
+        self.sumTillBot = self.sumBot(intN)
 
     def sumAllUnit(self,intN):
         if intN < 10:
@@ -10,6 +12,20 @@ class sumAll :
             last = intN % 10
             intN //= 10
             return last + self.sumAllUnit(intN)
+    
+    def sumBot(self,intN):
+        if intN < 10:
+            return intN
+        else :
+            intN = self.sumAllUnit(intN)
+            return self.sumBot(intN)
 
-num = sumAll(87)
+num = sumAll(77662)
+print(num.sum)
+
+#方法二：
+class solution:
+    def __init__(self,intN):
+        self.sum = sum(list(map(int,str(intN))))
+num = solution(77662)
 print(num.sum)
